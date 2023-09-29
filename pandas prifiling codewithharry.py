8ec3522f28c734132aa688ac984c54860ca91829
Satya Prakash Mohanty <mebubu03@gmail.com>
2023-09-29 12:25:07 +0530

    importing ydata-prifiling, pandas, openpyxl, install matplotlib==3.7 and 'fruit data.html' created.

---
+++ pandas prifiling codewithharry.py
@@ -0,0 +1,10 @@
+import pandas as pd
+# from pandas_profiling import ProfileReport # this is outdated
+from ydata_profiling import ProfileReport
+
+df = pd.read_excel('fruit_data_with_color.xlsx')
+print(df)
+
+# Generate a report
+profile = ProfileReport(df)
+profile.to_file(output_file="fruit data.html")
